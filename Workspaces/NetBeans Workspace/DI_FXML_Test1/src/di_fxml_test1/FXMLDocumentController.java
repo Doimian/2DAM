3bc7f54a339e6f9619e13f9ac9d2b92c1187a7fa
Doimian <damian.hermidaramoss@gmail.com>
2021-10-19 11:25:06 +0200

    19/10//11:30

---
+++ Workspaces/NetBeans Workspace/DI_FXML_Test1/src/di_fxml_test1/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di_fxml_test1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author usuario
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Button button;
+    @FXML
+    private Label label;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) 
+    {
+        System.out.println("Ha presionado el boton");
+        label.setText("Hello World!");
+    }
+    
+}
